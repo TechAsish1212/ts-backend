8d991354b5022cff24971e169182f13860756a9b
TechAsish1212 <gitasish1212@gmail.com>
2026-04-24 19:48:21 +0530

    feat(routes):create delete  routes

--- src/routes/book.route.ts
+++ src/routes/book.route.ts
@@ -1,12 +1,13 @@
-import  {  Router } from "express";
-import { addBooks, getBooks, updateBook } from "../controllers/book.controller";
+import { Router } from "express";
+import { addBooks, deleteBook, getBooks, updateBook } from "../controllers/book.controller";
 
 
-const bookRouter=Router();
+const bookRouter = Router();
 
 
-bookRouter.get('/get-book',getBooks);
-bookRouter.post('/add-book',addBooks);
-bookRouter.patch('/update-book/:id',updateBook);
+bookRouter.get('/get-book', getBooks);
+bookRouter.post('/add-book', addBooks);
+bookRouter.patch('/update-book/:id', updateBook);
+bookRouter.delete('/delete-book/:id', deleteBook);
 
 export default bookRouter;
