6b175108d2a32eea063e9df52761857b8b109227
TechAsish1212 <gitasish1212@gmail.com>
2026-04-24 19:19:58 +0530

    feat(controller):create update controller

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -1,14 +1,14 @@
 import { Request, Response } from "express";
 import { Book } from "../models/Book";
 
-export const getBooks = async(req: Request, res: Response) => {
+export const getBooks = async (req: Request, res: Response) => {
     try {
-        const books=await Book.find().sort({createdAt:-1});
+        const books = await Book.find().sort({ createdAt: -1 });
         return res.status(201).json({
-            success:true,
-            data:books
+            success: true,
+            data: books
         })
-    } catch (error:any) {
+    } catch (error: any) {
         return res.status(500).json({ success: false, error: error.messsage })
     }
 }
@@ -44,3 +44,39 @@ export const addBooks = async (req: Request, res: Response) => {
 
 
 }
+
+
+export const updateBook = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const { name, author, publishYear, description } = req.body;
+
+        const updateData: any = {};
+
+        if (name !== undefined) updateData.name = name;
+        if (author !== undefined) updateData.author = author;
+        if (publishYear !== undefined) updateData.publishYear = publishYear;
+        if (description !== undefined) updateData.description = description;
+
+        const book = await Book.findByIdAndUpdate(id, updateData, { new: true });
+
+        if (!book) {
+            return res.status(404).json({
+                success: false,
+                message: "Book Not Found"
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Update successful",
+            data: book
+        });
+
+    } catch (error: any) {
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
