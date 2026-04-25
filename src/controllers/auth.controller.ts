f8db65f9659720df95626387da008a17b2b62618
TechAsish1212 <gitasish1212@gmail.com>
2026-04-25 22:18:21 +0530

    feat(con):in signup function add only username will lowercase feature and signin fuction

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import e, { Request, Response } from "express";
 import { User } from "../models/User";
 import bcrypt from "bcrypt";
 import jwt from 'jsonwebtoken'
@@ -15,6 +15,15 @@ export const signup = async (req: Request, res: Response) => {
                 success: false,
                 message: "All fields are required"
             })
+        }
+
+        const isLowerCase = /^[a-z0-9_]+$/.test(username);
+
+        if (!isLowerCase) {
+            return res.status(400).json({
+                success: false,
+                message: "Username must be lowercase (a-z, 0-9, underscore only)"
+            });
         }
 
         const existingUser = await User.findOne({
@@ -51,6 +60,64 @@ export const signup = async (req: Request, res: Response) => {
             message: "User Registered Successfully",
             data: user,
             token
+        })
+
+    } catch (error: any) {
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+
+
+// signin
+
+export const signin = async (req: Request, res: Response) => {
+
+    const { username, email, password } = req.body;
+
+    try {
+
+        if ((!username && !email) || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "Username or Email and password are required"
+            })
+        }
+
+        const user = await User.findOne({
+            $or: [{ username }, { email }]
+        })
+
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User Not Found. Please signup first"
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid Password"
+            })
+        }
+
+        const token = jwt.sign(
+            { id: user._id, role: user.role },
+            process.env.JWT_SECRET_KEY as string,
+            { expiresIn: '7d' }
+        )
+
+        return res.status(201).json({
+            success: true,
+            message: "Login Successfully",
+            token,
+            data: user
         })
 
     } catch (error: any) {
