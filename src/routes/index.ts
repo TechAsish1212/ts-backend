de585a8d6862eff123bd90081b604554c30c21c3
TechAsish1212 <gitasish1212@gmail.com>
2026-04-25 20:42:57 +0530

    feat(route):create signup route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import bookRouter from "./book.route";
+import authRouter from "./auth.route";
 
 const routes=Router();
 
 
-routes.use('/book',bookRouter)
+routes.use('/book',bookRouter);
+routes.use('/auth',authRouter);
 
 export default routes;
